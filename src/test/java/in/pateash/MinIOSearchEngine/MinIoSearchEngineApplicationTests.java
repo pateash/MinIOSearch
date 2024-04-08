7b20baea8eb2efe87988b59344b1820122abf73c
Ashish Patel <ashishpatel0720@gmail.com>
2024-04-08 09:45:28 +0530

    Git: tests files added

---
+++ src/test/java/in/pateash/MinIOSearchEngine/MinIoSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pateash.MinIOSearchEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinIoSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
