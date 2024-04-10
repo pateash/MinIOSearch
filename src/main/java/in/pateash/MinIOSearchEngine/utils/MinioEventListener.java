5e9a20737c4589cb5fc3bcb30393095a123bc3b4
Ashish Patel <ashishpatel0720@gmail.com>
2024-04-11 01:22:56 +0530

    rabbitmq: adding setup script

--- src/main/java/in/pateash/MinIOSearchEngine/utils/MinioEventListener.java
+++ src/main/java/in/pateash/MinIOSearchEngine/utils/MinioEventListener.java
@@ -1,5 +1,7 @@
 package in.pateash.MinIOSearchEngine.utils;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +10,14 @@ public class MinioEventListener {
 
     @RabbitListener(queues = "minioQueue")
     public void receiveMessage(String message) {
-        // Process the message/event
-        System.out.println("Received MinIO event: " + message);
+        ObjectMapper mapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);;
+        try {
+            MinioEvent event = mapper.readValue(message, MinioEvent.class);
+            System.out.println("Event Name: " + event.getEventName());
+            System.out.println("Key: " + event.getKey());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
